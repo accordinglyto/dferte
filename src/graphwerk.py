574ea3429f54f15f0ec926f37bdff3fa5de34280
According <accordingtotheint@gmail.com>
2020-06-14 18:10:02 +0800

    D

--- src/graphwerk.py
+++ src/graphwerk.py
@@ -7,7 +7,7 @@ import matplotlib
 
 # Input your csv file here with historical data
 
-ad = genfromtxt('../financial_data/PSEI-index.csv', delimiter=',' ,dtype=str)
+ad = genfromtxt('../financial_data/BDO-BPI.csv', delimiter=',' ,dtype=str)
 pd = ad
 
 buy_dir = '../data/train/buy/'
