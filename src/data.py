2615c24f812ec1cc411ff16bb2a407e5aa4cde8f
According <accordingtotheint@gmail.com>
2020-06-23 21:20:56 +0800

    D

--- src/data.py
+++ src/data.py
@@ -1,20 +1,24 @@
 from psequant import get_pse_data
 
-df = get_pse_data("GTCAP", "2020-01-3", "2020-06-19")
-df.to_csv("file_name.csv")
+stock = "BDO"
+df = get_pse_data(stock, "2020-06-19", "2020-06-23")
+print(df)
 
 
-# read the data file in as a list
-fin = open("file_name.csv", "r")
-data_list = fin.readlines()
-fin.close()
-# test it ...
+# df.to_csv("file_name.csv")
 
-# remove list items from index 3 to 5 (inclusive)
-del data_list[0]
-# test it ...
 
-# write the changed data (list) to a file
-fout = open("file_name.csv", "w")
-fout.writelines(data_list)
-fout.close()
+# # read the data file in as a list
+# fin = open("file_name.csv", "r")
+# data_list = fin.readlines()
+# fin.close()
+# # test it ...
+
+# # remove list items from index 3 to 5 (inclusive)
+# del data_list[0]
+# # test it ...
+
+# # write the changed data (list) to a file
+# fout = open("file_name.csv", "w")
+# fout.writelines(data_list)
+# fout.close()
