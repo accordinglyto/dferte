b137a3d3981be4913a57cefb63fbd76396228c95
According <accordingtotheint@gmail.com>
2020-06-25 17:38:32 +0800

    D

--- src/13.py
+++ src/13.py
@@ -8,7 +8,6 @@ import matplotlib
 # Input your csv file here with historical data
 
 ad = genfromtxt(f"../financial_data/ICT.csv", delimiter=",", dtype=str)
-ad = ad[1500:]
 
 
 def convolve_sma(array, period):
@@ -30,9 +29,9 @@ def graphwerk(start, finish):
     c_close = []
     c_volume = []
     c_date = []
-    c_start = start + 12
+    c_start = start + 18
 
-    for x in range(finish - start):
+    for x in range(finish - start - 6):
         c_open.append(float(pd[c_start][1]))
         c_high.append(float(pd[c_start][2]))
         c_low.append(float(pd[c_start][3]))
@@ -59,7 +58,7 @@ def graphwerk(start, finish):
     min_forecast = min(c_low)
     max_forecast = max(c_high)
 
-    if close[-1] * 1.03 < max_forecast:
+    if close[-1] * 1.04 < max_forecast:
         decision = "buy"
     # for z in all_prices:
     # if close[-1] * 1.03 < z:
@@ -139,5 +138,5 @@ sell_dir = "../data/train/sell/"
 iter = 0
 
 for x in range(len(pd)):
-    graphwerk(iter, iter + 12)
+    graphwerk(iter, iter + 18)
     iter = iter + 2
